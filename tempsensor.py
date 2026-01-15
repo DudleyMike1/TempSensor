ffe7c9b76a5b181f06b86982f44b9db5eb6a0cf9
Mike Hollis <michael.hollis@dudleycol.ac.uk>
2026-01-15 10:05:01 +0000

    Temperature summary menu added

--- tempsensor.py
+++ tempsensor.py
@@ -18,10 +18,46 @@ def main():
 
 
 def tempData():
-    pass
+    dfData = getData()
+    print (dfData)
+    while True:
+        print("Temperatue Menu")
+        print("1. Output Daily Average Temperature")
+        print("2. Output Daily Maximum Temperature")
+        print("3. Output Daily Minimum Temperature")
+        print("4. Quit")
+        choice = input("What summary do you want?:")
+        if choice == "1":
+            averageTemp(dfData)
+        elif choice == "2":
+            maxTemp(dfData)
+        elif choice =="3":
+            minTemp(dfData)
+        elif choice == "4":
+            break
+        else:
+            print ("invalid selection")
 
 def vpdData():
+    dfData = getData()
+    print (dfData)
+
+def getData():
+    """Reads in the data from the file"""
+    df = pd.read_csv("Data\Meter1_data.csv")
+    return df
+
+def averageTemp(dfData):
     pass
+
+def maxTemp(dfData):
+    pass
+
+def minTemp(dfData):
+    pass
+
+
+
 
 
 main()
